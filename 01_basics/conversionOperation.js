cc593afda2e443795ef043496bd32e10ee21b712
Pranshu Tiwari <pranshutiwari560@gmail.com>
2025-06-30 14:32:43 +0530

    conversionOperations

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,141 @@
+
+let score = "Pranshu"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+// let valueInNumber = Number(score) //value ko type cast karna ho number me to explicitly number de denge to ho jaega
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber1);
+
+
+// "33" => 33 as number
+// "33abc" => NaN because it is not a number mixture of number and apalp
+//  true => 1; false => 0
+
+// let isLoggedIn = "pranshu"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => 'false' khali chodoge to false hi aaega
+// "pranshu" => true same valuepass ki h kyuki
+
+// let someNumber = 33
+
+// let stringNumber = String(someNumber) //idher string ke roop me jab forcefully type cast karna ho to String use karte h
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *********************** Operations *****************************
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue);//op= -3
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+// let str1 = "hello"
+// let str2 = " pranshu"
+
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);  op = 12
+// console.log(1 + "2");        12
+// console.log("1" + 2 + 2);    122
+// 
+// //string starting hogi to sko string me convert kar denge agar number starting me hoga to pehle operation hoga fir string add 
+
+
+// console.log(1 + 2 + "2");    32
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);  op =1 true means one and when incremented its get one again
+// console.log(+"");    op = 0 -> ""-empty and fir converison karenge to wo 0 hi reh jaega
+//dont use these these kind of thing these are not professional dont us confusin things
+
+// let num1, num2, num3
+
+// num1 = num2 = num3 = 2 + 2
+
+// let gameCounter = 100
+// ++gameCounter;
+// console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversionlet score = "pranshu"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
