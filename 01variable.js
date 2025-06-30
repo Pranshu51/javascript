c41a9cdb58be14161c6ac640f1d50731dbfd9690
Pranshu Tiwari <pranshutiwari560@gmail.com>
2025-06-30 11:39:38 +0530

    basics01

---
+++ 01variable.js
@@ -0,0 +1,22 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2 // not allowed
+
+
+accountEmail = "pr@hc.com"
+accountPassword = "22121"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])//iise  multile varibae ek saath print karwa skte h aur ek table form me convert ho jaegi
