ea7eda766345687c13f12a9998f9630edfebc278
Pranshu Tiwari <pranshutiwari560@gmail.com>
2025-06-30 11:52:09 +0530

    datatypes

---
+++ datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version (everything is running in new version)
+
+// alert( 3 + 3) // we are using nodejs, not browser ye browser me chalega ya html link karke chlega nodejs me chlta h but different syntax h
+
+console.log(3 
+    +
+     3) // code readability should be high not a good practice
+
+console.log("pranshu")//also dont use semicolon readibility kharab hoti hai
+
+
+let name = "pranshu"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value 
+// undefined => eg temperature
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
